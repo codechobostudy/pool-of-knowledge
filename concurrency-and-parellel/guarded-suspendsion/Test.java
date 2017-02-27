895466862cd43e13de9e4825ac21db3689b76c7c
myungsung Jung <myungsung.jung@how2marry.com>
2017-02-27 14:02:36 +0900

    Guarded Suspention Pattern

---
+++ concurrency-and-parellel/guarded-suspendsion/Test.java
@@ -0,0 +1,9 @@
+package multithread.guaredSuspension;
+
+public class Test {
+  public static void main(String[] args) {
+    RequestQueue requestQueue = new RequestQueue();
+    new ClientThread(requestQueue, "Alice", 3141592L).start();
+    new ServerThread(requestQueue, "Bobby", 6535897L).start();
+  }
+}
